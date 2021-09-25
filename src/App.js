da5150905d9b0ab7fc20c6c308a3ca8fc819cf0c
neha hussain <nehahussain347@gmail.com>
2021-09-25 17:05:18 +0500

    main screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+    const handleSubmit = event => {
+   event.preventDefault();
+   alert('You have submitted the form.')
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="formdiv">
+      <h1>
+        calculate deivery time
+      </h1>
+      <form  onSubmit={handleSubmit}>
+      <fieldset>
+      <label for="order_date_time"><p>Order (date and time):</p>
+      <input type="datetime-local" id="order_date_time" name="order_date_time" /></label>
+      
+         <label>
+           <p>Order Type</p>
+           <select name="order_type" >
+               <option value="">--Please choose an option--</option>
+               <option value="urgent">urgent</option>
+               <option value="normal">normal</option>
+           </select>
+         </label>
+         <label>
+           <p>Country</p>
+           <select name="country" >
+               <option value="">--Please choose an option--</option>
+               <option value="uae">UAE</option>
+               <option value="Pakistan">Pakistan</option>
+           </select>
+         </label>
+       </fieldset>
+       <button type="submit">Calculate</button>
+      </form>
     </div>
   );
 }
